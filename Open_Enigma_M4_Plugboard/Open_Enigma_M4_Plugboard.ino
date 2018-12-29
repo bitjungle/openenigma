9781827d6ddfddc6cd363d66d83e8faaac273ba8
bitjungle <devel@bitjungle.com>
2018-12-29 20:27:43 +0100

    More code formatting and some documentation

--- Open_Enigma_M4_Plugboard/Open_Enigma_M4_Plugboard.ino
+++ Open_Enigma_M4_Plugboard/Open_Enigma_M4_Plugboard.ino
@@ -33,7 +33,7 @@ int procesval = 0;
 int procesvala = 0;
 int mode = 0;
 unsigned long mtime;
-int mdex =0;
+int mdex = 0;
 int behavior = 1;
 int plugred = 0;
 
@@ -48,7 +48,7 @@ int dig4 = 37;
 
 int data[3][40] ={ {36,36,36,36,18,39,19,36,6,4,14,19,17,14,13,8,2,18,36,4,13,8,6,12,0,36,12,0,17,10,36,30,36,13,3,19,36,36,36,36},
                   {36,36,36,36,18,39,19,36,6,4,14,19,17,14,13,8,2,18,36,4,13,8,6,12,0,36,12,0,17,10,36,30,36,36,3,19,36,36,36,36},
-                  {36,36,36,36,18,39,19,36,6,4,14,19,17,14,13,8,2,18,36,4,13,8,6,12,0,36,12,0,17,10,36,29,36,36,3,19,36,36,36,36} };
+                  {36,36,36,36,18,39,19,36,6,4,14,19,17,14,13,8,2,18,36,4,13,8,6,12,0,36,12,0,17,10,36,29,36,36,3,19,36,36,36,36}};
 
 // Define the 16-Segments Pins as 2 Arrays
 int segment[17] = {24,22,25,31,38,36,32,30,28,26,23,27,33,35,34,29,37}; //cathode array
@@ -114,7 +114,7 @@ int led5 = 48;
 
                                       //4,10,12, 5,11, 6, 3,16,21,25,13,19,14,22,24, 7,23,20,18,15, 0, 8, 1,17, 2, 9
 //Define the rotor values                 A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q
-static const int rotorvals[12][78] = { {  4,10,12, 5,11, 6, 3,16,21,25,13,19,14,22,24, 7,123,20,18,15, 0, 8, 1,17, 2, 9,
+static const int rotorvals[12][78] = { { 4,10,12, 5,11, 6, 3,16,21,25,13,19,14,22,24, 7,123,20,18,15, 0, 8, 1,17, 2, 9,
                                          4,10,12, 5,11, 6, 3,16,21,25,13,19,14,22,24, 7,123,20,18,15, 0, 8, 1,17, 2, 9,
                                          4,10,12, 5,11, 6, 3,16,21,25,13,19,14,22,24, 7,123,20,18,15, 0, 8, 1,17, 2, 9 },  // wheel 1
 
@@ -161,7 +161,7 @@ static const int rotorvals[12][78] = { {  4,10,12, 5,11, 6, 3,16,21,25,13,19,14,
                                        { 17,3,14,1,9,13,19,10,21,4,7,12,11,5,2,22,25,0,23,6,24,8,15,18,20,16,
                                          17,3,14,1,9,13,19,10,21,4,7,12,11,5,2,22,25,0,23,6,24,8,15,18,20,16,
                                          17,3,14,1,9,13,19,10,21,4,7,12,11,5,2,22,25,0,23,6,24,8,15,18,20,16 }    // = UKW-C 
-                                        };
+                                      };
                                         
                                     
 static const int rotorvali[10][78] = { { 20,22,24, 6, 0, 3, 5,15,21,25, 1, 4, 2,10,12,19, 7,23,18,11,17, 8,13,16,14, 9,
@@ -203,7 +203,8 @@ static const int rotorvali[10][78] = { { 20,22,24, 6, 0, 3, 5,15,21,25, 1, 4, 2,
                                          
                                        { 4,11,15,25,7,0,23,9,13,24,3,17,10,5,2,19,18,8,1,12,6,22,16,21,14,20,
                                          4,11,15,25,7,0,23,9,13,24,3,17,10,5,2,19,18,8,1,12,6,22,16,21,14,20,
-                                         4,11,15,25,7,0,23,9,13,24,3,17,10,5,2,19,18,8,1,12,6,22,16,21,14,20 } }; //Gamma i
+                                         4,11,15,25,7,0,23,9,13,24,3,17,10,5,2,19,18,8,1,12,6,22,16,21,14,20 } 
+                                      }; //Gamma i
                                                                                 
                                         
 // Define a 2D Array for keeping the wheel locations & positions
@@ -215,7 +216,7 @@ int plugval [2][26] = {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                        {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25}};
 int pluguse = 0; int paindex = 0; int pbindex = 1;
 int prindex = 0;
-int prtindex =0;
+int prtindex = 0;
 void setup() {
 // Initialize all 29 LED pins as Output  
   for (int index = 0; index <= 2; index++) {
@@ -264,6 +265,9 @@ void setup() {
 
 }
 
+/**
+ * Main loop
+ */
 void loop() {
  
 // Keyboard debounce & test for new key pressed  
@@ -271,7 +275,13 @@ void loop() {
   if (time > otime + 100) {keyval = readkbde();}
   if(keyval == kvalo) {windex = 0;}
   kvalo = keyval;
-  if ((mode == 0) && (keyval == 46) && (windex ==1)){ behavior ++; windex =0; if(behavior > 2) {behavior = 0; } }
+  if ((mode == 0) && (keyval == 46) && (windex ==1)) { 
+    behavior ++; 
+    windex = 0; 
+    if(behavior > 2) {
+      behavior = 0; 
+    } 
+  }
   if((keyval == 45) && (windex ==1)) {modeselect();}
 // The whole Enigma machine operation revolves around which Operating Mode is current  
   if(mode == 0) {mode0();}
@@ -280,70 +290,79 @@ void loop() {
   else if(mode == 3) {mode3();}
   else if(mode == 4) {mode4();}
   else if(mode == 5) {mode5();}
-  else {    }
+  else {/* Do nothing */}
 
 Serial.println(keyval); // for debuging prints keybord value to serial monitor// for run or del
-
 }
 
-// This function takes care of figuring out which key has been pressed & returns a unique Integer
- int readkbde() {
- int kval = 100;
- for (int index = 0; index <= 3; index++) { inval[index] = analogRead(inpin[index]);   } //Reads analog input values
- if((inval[0] > 982) && (inval[1] > 973) && (inval[2] > 966) && (inval[3] > 973))  {kval = 100;} // no key press
- else if((inval[0] < 981) && (inval[0] > 903)) {kval = 49;} //up arrow 4
- else if((inval[0] < 902) && (inval[0] > 831)) {kval = 48;} //up arrow 3
- else if((inval[0] < 830) && (inval[0] > 762)) {kval = 47;} //up arrow 2
- else if((inval[0] < 761) && (inval[0] > 693)) {kval = 46;} //up arrow 1
- else if((inval[0] < 692) && (inval[0] > 622)) {kval = 45;} //mode
- else if((inval[0] < 621) && (inval[0] > 545)) {kval = 44;} //enter
- else if((inval[0] < 544) && (inval[0] > 458)) {kval = 43;}
- else if((inval[0] < 457) && (inval[0] > 358)) {kval = 42;}
- else if((inval[0] < 357) && (inval[0] > 237)) {kval = 41;}
- else if(inval[0] < 236 ) {kval = 40;}
- else if((inval[1] < 972) && (inval[1] > 875)) {kval = 16;}
- else if((inval[1] < 874) && (inval[1] > 784)) {kval = 22;}
- else if((inval[1] < 783) && (inval[1] > 696)) {kval = 4;}
- else if((inval[1] < 695) && (inval[1] > 605)) {kval = 17;}
- else if((inval[1] < 604) && (inval[1] > 507)) {kval = 19;}
- else if((inval[1] < 506) && (inval[1] > 395)) {kval = 25;}
- else if((inval[1] < 394) && (inval[1] > 261)) {kval = 20;}
- else if((inval[1] < 260) && (inval[1] > 94)) {kval = 8;}
- else if(inval[1] < 93 ) {kval = 14;}
- else if((inval[2] < 965) && (inval[2] > 855)) {kval = 0;}
- else if((inval[2] < 854) && (inval[2] > 752)) {kval = 18;}
- else if((inval[2] < 751) && (inval[2] > 650)) {kval = 3;}
- else if((inval[2] < 649) && (inval[2] > 542)) {kval = 5;}
- else if((inval[2] < 541) && (inval[2] > 421)) {kval = 6;}
- else if((inval[2] < 420) && (inval[2] > 278)) {kval = 7;}
- else if((inval[2] < 277) && (inval[2] > 100)) {kval = 9;}
- else if(inval[2] < 99 ) {kval = 10;}
- else if((inval[3] < 972) && (inval[3] > 875)) {kval = 15;}
- else if((inval[3] < 874) && (inval[3] > 784)) {kval = 24;}
- else if((inval[3] < 783) && (inval[3] > 696)) {kval = 23;}
- else if((inval[3] < 695) && (inval[3] > 605)) {kval = 2;}
- else if((inval[3] < 604) && (inval[3] > 507)) {kval = 21;}
- else if((inval[3] < 506) && (inval[3] > 395)) {kval = 1;}
- else if((inval[3] < 394) && (inval[3] > 261)) {kval = 13;}
- else if((inval[3] < 260) && (inval[3] > 94)) {kval = 12;}
- else if(inval[3] < 93 ) {kval = 11;}
- else {kval = 100;}
-if(kval < 99) {otime = millis();}
-
-             //Starts key debounce timer
- if((kval >= 0) && (kval <= 99)) {windex = 1;}  //windex showing true (1) indicates the return of a fresh key stroke
- Serial.println(kval);
- return kval;
+/**
+ * This function takes care of figuring out which key has been pressed
+ * @return int A unique integer for the key pressed
+ */
+int readkbde() {
+  int kval = 100;
+  for (int index = 0; index <= 3; index++) { //Read analog input values
+    inval[index] = analogRead(inpin[index]);   
+   } 
+  if((inval[0] > 982) && (inval[1] > 973) && (inval[2] > 966) && (inval[3] > 973))  {kval = 100;} // no key press
+  else if((inval[0] < 981) && (inval[0] > 903)) {kval = 49;} //up arrow 4
+  else if((inval[0] < 902) && (inval[0] > 831)) {kval = 48;} //up arrow 3
+  else if((inval[0] < 830) && (inval[0] > 762)) {kval = 47;} //up arrow 2
+  else if((inval[0] < 761) && (inval[0] > 693)) {kval = 46;} //up arrow 1
+  else if((inval[0] < 692) && (inval[0] > 622)) {kval = 45;} //mode
+  else if((inval[0] < 621) && (inval[0] > 545)) {kval = 44;} //enter
+  else if((inval[0] < 544) && (inval[0] > 458)) {kval = 43;}
+  else if((inval[0] < 457) && (inval[0] > 358)) {kval = 42;}
+  else if((inval[0] < 357) && (inval[0] > 237)) {kval = 41;}
+  else if(inval[0] < 236 ) {kval = 40;}
+  else if((inval[1] < 972) && (inval[1] > 875)) {kval = 16;}
+  else if((inval[1] < 874) && (inval[1] > 784)) {kval = 22;}
+  else if((inval[1] < 783) && (inval[1] > 696)) {kval = 4;}
+  else if((inval[1] < 695) && (inval[1] > 605)) {kval = 17;}
+  else if((inval[1] < 604) && (inval[1] > 507)) {kval = 19;}
+  else if((inval[1] < 506) && (inval[1] > 395)) {kval = 25;}
+  else if((inval[1] < 394) && (inval[1] > 261)) {kval = 20;}
+  else if((inval[1] < 260) && (inval[1] > 94)) {kval = 8;}
+  else if(inval[1] < 93 ) {kval = 14;}
+  else if((inval[2] < 965) && (inval[2] > 855)) {kval = 0;}
+  else if((inval[2] < 854) && (inval[2] > 752)) {kval = 18;}
+  else if((inval[2] < 751) && (inval[2] > 650)) {kval = 3;}
+  else if((inval[2] < 649) && (inval[2] > 542)) {kval = 5;}
+  else if((inval[2] < 541) && (inval[2] > 421)) {kval = 6;}
+  else if((inval[2] < 420) && (inval[2] > 278)) {kval = 7;}
+  else if((inval[2] < 277) && (inval[2] > 100)) {kval = 9;}
+  else if(inval[2] < 99 ) {kval = 10;}
+  else if((inval[3] < 972) && (inval[3] > 875)) {kval = 15;}
+  else if((inval[3] < 874) && (inval[3] > 784)) {kval = 24;}
+  else if((inval[3] < 783) && (inval[3] > 696)) {kval = 23;}
+  else if((inval[3] < 695) && (inval[3] > 605)) {kval = 2;}
+  else if((inval[3] < 604) && (inval[3] > 507)) {kval = 21;}
+  else if((inval[3] < 506) && (inval[3] > 395)) {kval = 1;}
+  else if((inval[3] < 394) && (inval[3] > 261)) {kval = 13;}
+  else if((inval[3] < 260) && (inval[3] > 94)) {kval = 12;}
+  else if(inval[3] < 93 ) {kval = 11;}
+  else {kval = 100;}
+  if(kval < 99) {otime = millis();}
+  
+  //Starts key debounce timer
+  if ((kval >= 0) && (kval <= 99)) {windex = 1;}  //windex showing true (1) indicates the return of a fresh key stroke
+  Serial.println(kval);
+  return kval;
 }
 
-// Function to change operating Mode
+/**
+ * Function to change operating Mode
+ */
 void modeselect() {
   mode++;
   if(mode >=6) {mode = 0;}
   windex = 0;
 }
 
-// Default Mode: Enigma is a Typewriter
+/**
+ * Mode 0
+ * Default Mode: Enigma is a typewriter
+ */
 void mode0() {
   if ((keyval >= 0) && (keyval <= 25)) {
     lampval = keyval;
@@ -356,7 +375,10 @@ void mode0() {
   marquee();
 }
 
-// Select the Rotors & the Reflector  
+/**
+ * Mode 1
+ * Select the rotors & the reflector  
+ */
 void mode1() {
   int index;
   digitalWrite(led1, HIGH);
@@ -386,7 +408,8 @@ void mode1() {
       for (index = wheel[2][0]; (index == wheel[1][0]) || (index == wheel[0][0]) || (index == wheel[2][0]); index++) {
         if(index > 33) {index = 26;} 
       }
-      wheel[2][0] = index;  windex = 0;
+      wheel[2][0] = index;  
+      windex = 0;
     }
   }
 
@@ -396,7 +419,9 @@ void mode1() {
         if (index > 33) {
           index = 26;
         } 
-      } wheel[1][0] = index;  windex = 0;
+      } 
+      wheel[1][0] = index;  
+      windex = 0;
     }
   }
   if (windex == 1) {
@@ -405,7 +430,9 @@ void mode1() {
         if (index > 33) {
           index = 26;
         } 
-      } wheel[0][0] = index;  windex = 0;
+      } 
+      wheel[0][0] = index;  
+      windex = 0;
     }
   }
   if (windex == 1) {
@@ -414,7 +441,9 @@ void mode1() {
         if (index < 28) {
           index = 35;
         } 
-      } wheel[2][0] = index;  windex = 0;
+      } 
+      wheel[2][0] = index;  
+      windex = 0;
     }
   }
   if (windex == 1) {
@@ -423,7 +452,9 @@ void mode1() {
         if (index < 28) {
           index = 35;
         } 
-      } wheel[1][0] = index;  windex = 0;
+      } 
+      wheel[1][0] = index;  
+      windex = 0;
     }
   }
   if (windex == 1) {
@@ -432,7 +463,9 @@ void mode1() {
         if (index < 28) {
           index = 35;
         } 
-      } wheel[0][0] = index;  windex = 0;
+      } 
+      wheel[0][0] = index;  
+      windex = 0;
     }
   }
     
@@ -464,7 +497,10 @@ void mode1() {
   digitalWrite(led1, LOW);
 }
 
-// Position the Inner setting of each Rotor  
+/**
+ * Mode 2
+ * Position the inner setting of each rotor  
+ */
 void mode2() {
   digitalWrite(led2, HIGH);
   if (windex == 1) {
@@ -542,7 +578,10 @@ void mode2() {
   digitalWrite(led2, LOW);
 }
 
-// Position the Start character of each Wheel  
+/**
+ * Mode 3
+ * Position the Start character of each Wheel  
+ */
 void mode3() {
   digitalWrite(led3, HIGH);
   if (windex == 1) {
@@ -617,7 +656,10 @@ void mode3() {
   digitalWrite(led3, LOW);
 }
 
-// Define the Plugboard pairs  
+/**
+ * Mode 4
+ * Define the Plugboard pairs  
+ */
 void mode4() {
   if(plugred == 0) {readplugs(); }
   int index = 0;
@@ -693,7 +735,10 @@ void mode4() {
   digitalWrite(led4, LOW);
 }
 
-// This is Normal Operation Mode to Encrypt/Decrypt  
+/**
+ * Mode 5
+ * This is normal operation mode to Encrypt/Decrypt  
+ */
 void mode5() {
   int pv = 0;
   digitalWrite(led5, HIGH);
@@ -817,7 +862,9 @@ void mode5() {
   digitalWrite(led5, LOW);
 }
 
-// Helper Function to light the proper key 
+/**
+ * Helper Function to light the proper key 
+ */
 void lampita() {
   if(lampval <= 25) {
   digitalWrite(lamparray[lampval][0],0);
@@ -826,6 +873,9 @@ void lampita() {
   }
 }
 
+/**
+ * 
+ */
 void lampitb(){
   if(lampval <= 25) {
     digitalWrite(lamparray[lampval][0],1);
@@ -833,7 +883,9 @@ void lampitb(){
   }
 }
 
-// Help Function to illuminate "Nixies" 
+/** 
+ *  Send characters to the four 16-segment LED's
+ */
 void nixisend() {
   sixteenSegWrite(0, dig1);
   sixteenSegWrite(1, dig2);
@@ -841,6 +893,9 @@ void nixisend() {
   sixteenSegWrite(3, dig4);
 }
 
+/**
+ * Display the marquee text defined in the data[] array on the LED segments
+ */
 void marquee() {  
   time = millis();
   if( mtime < time) {
@@ -855,7 +910,11 @@ void marquee() {
   nixisend(); 
 }
 
-// Function that actually turns on each of 17 appropriate segments on each "Nixie"
+/**
+ * Function that actually turns on each of 17 appropriate segments on each 16-segment LED
+ * @param int digit
+ * @param int character
+ */
 void sixteenSegWrite(int digit, int character) {
   digitalWrite(anode[digit],0);
   for (int index = 0; index < 17; index++) {
@@ -868,12 +927,19 @@ void sixteenSegWrite(int digit, int character) {
   digitalWrite(anode[digit], 1);
 }
 
+/**
+ * Send the characters D O N E to the LED display, 
+ * and reset the dig[1-4] vars to 37.
+ */
 void done() {
   dig1 = 3; dig2 = 14; dig3 = 13; dig4 = 4;
   nixisend();
-  dig1 =37; dig2 = 37; dig3 = 37; dig4 = 37;
+  dig1 = 37; dig2 = 37; dig3 = 37; dig4 = 37;
 }
-    
+
+/**
+ * 
+ */   
 void indexwheels() {
   if (behavior > 0) { 
     if (rotorvals[wheel[1][0]-27][wheel[1][2]] >= 100) {
@@ -902,6 +968,9 @@ void indexwheels() {
   } 
 }
 
+/**
+ * 
+ */   
 void readplugs() {
   for (int index = 0 ; index <= 24; index++) {
   
@@ -921,9 +990,3 @@ void readplugs() {
   }
   plugred = 1;
 }
-     
-     
-     
-     
-     
-  
